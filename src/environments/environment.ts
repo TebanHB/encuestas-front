d2cf955854db4c377a2c713abb8ece901ba07043
Esteban <68796932+TebanHB@users.noreply.github.com>
2026-03-17 09:30:56 -0400

    Update apiBaseUrl for development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://3850-181-115-208-107.ngrok-free.app'
+    apiBaseUrl: 'https://c72f-2800-cd0-440d-e400-d401-5bf-2b06-cf33.ngrok-free.app'
 };
